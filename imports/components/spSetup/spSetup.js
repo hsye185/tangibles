a7e437763a173f0665e2da6fa7631a615152b30d
JCSanPedro <j.carlosanpedro@gmail.com>
2016-10-04 16:18:48 +1300

    Added grade select box which is populated from existing modules

--- imports/components/spSetup/spSetup.js
+++ imports/components/spSetup/spSetup.js
@@ -4,11 +4,32 @@ import template from './spSetup.html';
 import {TangibleController} from '../../api/tangibles/controller';
 import {Diagrams} from '../../api/collections/diagrams.js';
 import {Libraries} from '../../api/collections/libraries.js';
+import {Modules} from '../../api/collections/modules.js';
 
 class SPSetupCtrl {
     constructor($scope, $reactive, $stateParams, $tgImages, $state, $tgSharedData, $const) {
         'ngInject';
-        $reactive(this).attach($scope);  
+        $reactive(this).attach($scope);
+
+        $scope.modules = [];
+        this.subscribe('modules');
+        this.helpers({
+            modules() {
+                return Modules.find({});
+            }
+        });
+        var millisecondsToWait = 100;
+        setTimeout(function() {
+            let moduleList = Modules.find({}).fetch();
+            let strings = [];
+            for(let i = 0; i<moduleList.length; i++){
+                $scope.modules.push(moduleList[i]);
+            }
+            // Meteor.call("modules.deleteEntireCollection", null);
+            $scope.$apply();
+            // $scope.modules = cursor.toArray();
+        }, millisecondsToWait, $scope);
+
     }
 }
 
