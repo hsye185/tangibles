3aad3d13ae1ee31e879287469a5c0dc4ed5fb422
Haseeb Syed <hsye185@aucklanduni.ac.nz>
2016-10-20 16:06:44 +1300

    fixed setup page, added check for name and grade

--- imports/components/spSetup/spSetup.js
+++ imports/components/spSetup/spSetup.js
@@ -29,52 +29,57 @@ class SPSetupCtrl {
 
         $scope.$gameStateService = $gameStateService;
         $scope.playerName = ''; 
-        $scope.module = '';
+        $scope.module = null;
 
         $scope.submit = function() {
             setTimeout(function() {
-                let pName = $scope.playerName.toLowerCase();
-                let chosenModuleName = $scope.module.name;
-                let student = Students.findOne({_id: pName});
-                if(!student){
-                    student = {
-                        _id: pName,
-                        moduleProgress: {}
-                    };
-                    student.moduleProgress[chosenModuleName] = [1,0,0,0,0,0,0,0,0];
-                    Meteor.call("students.insert", student);
-                }else{
-                    if(!student.moduleProgress[chosenModuleName]){
+                if($scope.playerName.length > 0 && $scope.module != null){
+                    let pName = $scope.playerName.toLowerCase();
+                    let chosenModuleName = $scope.module.name;
+                    let student = Students.findOne({_id: pName});
+                    if(!student){
+                        student = {
+                            _id: pName,
+                            moduleProgress: {}
+                        };
                         student.moduleProgress[chosenModuleName] = [1,0,0,0,0,0,0,0,0];
-                        Meteor.call("students.updateModuleProgress", student);
+                        Meteor.call("students.insert", student);
+                    }else{
+                        if(!student.moduleProgress[chosenModuleName]){
+                            student.moduleProgress[chosenModuleName] = [1,0,0,0,0,0,0,0,0];
+                            Meteor.call("students.updateModuleProgress", student);
+                        }
                     }
-                }
-                let words = Modules.findOne({name: chosenModuleName});
-                for(m of $scope.modules){
-                    if(m.name == chosenModuleName){
-                        $scope.$gameStateService.currentModuleWords = m.words;
-                        break;
+                    let words = Modules.findOne({name: chosenModuleName});
+                    for(m of $scope.modules){
+                        if(m.name == chosenModuleName){
+                            $scope.$gameStateService.currentModuleWords = m.words;
+                            break;
+                        }
                     }
+                    $scope.$gameStateService.currentStudent = student;
+                    $scope.$gameStateService.currentModuleName = chosenModuleName;
                 }
-                $scope.$gameStateService.currentStudent = student;
-                $scope.$gameStateService.currentModuleName = chosenModuleName;
             }, 100, $scope);
-            $(function () {
-                $('#content').fadeOut(1000,function(){
-                    $(function () {
-                        // alert();
+            if($scope.playerName.length > 0 && $scope.module != null){
+                $(function () {
+                    $('#content').fadeOut(1000,function(){
+                        $(function () {
+                            // alert();
 
-                        topHeight = window.innerHeight - 140;
-                        midHeight = window.innerHeight - 40;
-                        $('#topPanelAlpha').animate({height: '100px'},1000);
-                        $('#topPanel').animate({top: '100px', height: topHeight},1000);
-                        $('#midPanel').animate({bottom: '0px', left: '0px', height: '40px', width: '25%'},1000);
-                        $('#botPanel').animate({bottom: '0px', left: '25%', width: '75%', height: '40px'},1000, function(){
-                            $state.go("levelSelect");
+                            topHeight = window.innerHeight - 140;
+                            midHeight = window.innerHeight - 40;
+                            $('#topPanelAlpha').animate({height: '100px'},1000);
+                            $('#topPanel').animate({top: '100px', height: topHeight},1000);
+                            $('#midPanel').animate({bottom: '0px', left: '0px', height: '40px', width: '25%'},1000);
+                            $('#botPanel').animate({bottom: '0px', left: '25%', width: '75%', height: '40px'},1000, function(){
+                                $state.go("levelSelect");
+                            });
                         });
                     });
                 });
-            });
+            }
+            
         }
 
         $scope.gotoHome = function(){
